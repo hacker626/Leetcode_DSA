ade804e82af3bee291813c61244f2b15936d86bc
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-01 15:03:08 +0530

    Time: 1358 ms (42.17%) | Memory: 16.4 MB (21.27%) - LeetSync

--- fair-distribution-of-cookies/fair-distribution-of-cookies.py
+++ fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -1,23 +1,41 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
-
-        def dfs(i, mx):
-  
-            if mx >= self.ans:
-                return
-
-            if i == len(cookies):
-                self.ans = min(self.ans, mx)
-                return
-
-            for j in range(k):
-                kids[j] += cookies[i]
-                dfs(i + 1, max(mx, kids[j]))
-                kids[j] -= cookies[i]
+        if(k == len(cookies)):
+            return max(cookies)
+        dp = {}
+        ans = [0 for i in range(k)]
+        res = [float('inf')]
+        self.dfs(cookies,dp,len(cookies),k,ans,res,0)
+        return res[0]
+    def dfs(self,cookies,dp,n,k,ans,res,temp):
+        if(n == 0):
+            # print(ans,res)
+            # print(temp,max(ans),ans)
+            res[0] = min(res[0],temp)
             return
+        for i in range(k):
+            ans[i]+=cookies[n-1]
+            count_ = temp
+            temp = max(temp,ans[i])
+            self.dfs(cookies,dp,n-1,k,ans,res,temp)
+            if(temp == ans[i]):
+                temp = count_
+            ans[i]-=cookies[n-1]
+# class Solution:
+#     def distributeCookies(self, cookies: List[int], k: int) -> int:
+#         dp = {}
+#         ans = [0 for i in range(k)]
+#         res = [float('inf')]
+#         self.dfs(cookies,dp,len(cookies),k,ans,res)
+#         return res[0]
+#     def dfs(self,cookies,dp,n,k,ans,res):
+#         if(n == 0):
+#             # print(ans,res)
+#             res[0] = min(res[0],max(ans))
+#             return
+#         for i in range(k):
+#             ans[i]+=cookies[n-1]
+#             self.dfs(cookies,dp,n-1,k,ans,res)
+#             ans[i]-=cookies[n-1]
 
-        kids, self.ans = defaultdict(int), inf
-        cookies.sort(reverse=True)
-        dfs(0,0)
-
-        return self.ans
+        

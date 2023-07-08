9538e0f9c44d4e72294b2a22a5d20f1b184a931f
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-09 00:19:16 +0530

    Time: 461 ms (6.30%) | Memory: 18.9 MB (80.04%) - LeetSync

--- longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,18 +1,31 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        low,high = 0,0
-        index0 = 0
-        ans = float('-inf')
-        while(high<len(nums)):
-            if(nums[high] == 0):
-                index0+=1
-            if(index0 == 2):
-                ans = max(ans,high-low-1)
-                while(nums[low] != 0):
-                    low+=1
-                low+=1
-                index0-=1
-            high+=1
-        return max(ans,high-low-1)
+        cnt,prvCnt = 0,0
+        res = 0
+        for i in range(len(nums)):
+            if(nums[i] == 1):
+                cnt+=1
+            else:
+                res = max(res,cnt+prvCnt)
+                prvCnt = cnt
+                cnt =  0
+        
+        ans = max(res,cnt+prvCnt)
+        return ans-1 if(ans == len(nums)) else ans
+
+        # low,high = 0,0
+        # index0 = 0
+        # ans = float('-inf')
+        # while(high<len(nums)):
+        #     if(nums[high] == 0):
+        #         index0+=1
+        #     if(index0 == 2):
+        #         ans = max(ans,high-low-1)
+        #         while(nums[low] != 0):
+        #             low+=1
+        #         low+=1
+        #         index0-=1
+        #     high+=1
+        # return max(ans,high-low-1)
 
                     

9df94937270f3c06a85674f6d90cd5f6b38aa14a
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-08 17:48:31 +0530

    Time: 302 ms (62.24%) | Memory: 17.8 MB (97.33%) - LeetSync

--- find-k-closest-elements/find-k-closest-elements.py
+++ find-k-closest-elements/find-k-closest-elements.py
@@ -1,5 +1,4 @@
 import heapq
-
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         low,high = 0,len(arr)
@@ -8,16 +7,5 @@ class Solution:
                 low+=1
             else:
                 high-=1
-        return [i for i in arr[low:high]]
-
-
-        # maxHeap = []
-        # for i in range(len(arr)):
-        #     heapq.heappush(maxHeap,[-abs(x-arr[i]),-arr[i],arr[i]])
-        #     if(len(maxHeap)>k):
-        #         heapq.heappop(maxHeap)
-
-        # ans = []
-        # for i in maxHeap:
-        #     ans.append(i[2])
-        # return sorted(ans)
+        return arr[low:high]
+        # ffffff

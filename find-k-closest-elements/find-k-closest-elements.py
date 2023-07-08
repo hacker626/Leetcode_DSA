d9e899328110e9bab4c7ef97d82809523830e073
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-08 13:57:52 +0530

    Time: 441 ms (5.02%) | Memory: 18.8 MB (10.58%) - LeetSync

--- find-k-closest-elements/find-k-closest-elements.py
+++ find-k-closest-elements/find-k-closest-elements.py
@@ -5,29 +5,10 @@ class Solution:
         maxHeap = []
         for i in range(len(arr)):
             heapq.heappush(maxHeap,[-abs(x-arr[i]),-arr[i],arr[i]])
-            # print(maxHeap)
             if(len(maxHeap)>k):
                 heapq.heappop(maxHeap)
-                # print("Popped: ",maxHeap)
-        # dict_ = {}
-        # print(maxHeap)
+
         ans = []
         for i in maxHeap:
             ans.append(i[2])
         return sorted(ans)
-        # while(len(maxHeap)>0):
-        #     temp = heapq.heappop(maxHeap)
-        #     if(temp not in dict_):
-        #         dict_[temp] = 1
-        #     else:
-        #         dict_[temp] += 1
-        # print(dict_)
-        # ans = []
-        # for i in dict_:
-        #     if(dict_[i] == 1):
-        #         ans.append(x+abs(i))
-        #     else:
-        #         ans+=[x+abs(i),x-abs(i)]
-        
-        # return sorted(ans)
-        # print(maxHeap)

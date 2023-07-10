351cddf79e67ae5e565a0ea0f41896eee0525946
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-10 12:19:57 +0530

    Time: 666 ms (9.22%) | Memory: 57.2 MB (43.06%) - LeetSync

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -12,10 +12,11 @@ class Solution:
     def inOrder(self,root,ans):
         if(root == None):
             return 0
-        if(root.left):
-            ans = min(ans,1+self.inOrder(root.left,ans))
         if(root.left ==None and root.right == None):
             return 1
+        if(root.left):
+            ans = min(ans,1+self.inOrder(root.left,ans))
+        
         if(root.right):
             ans = min(ans,1+self.inOrder(root.right,ans))
 

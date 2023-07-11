3c2616fcba5a4ca74924120c8437244d65fca5ca
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-11 07:36:56 +0530

    Time: 30 ms (21.37%) | Memory: 13.7 MB (47.33%) - LeetSync

--- all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -14,47 +14,38 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        graph = {}
-        # temp = {}
+        graph,vis = {},{}
+        ans = []
     
         self.inOrder(root,graph)
-        # print(graph)
-        vis = {}
         for i in graph:
             vis[i] = -1
-        # for i in graph[target]:
-        #     print(i)
-        ans = []
         vis[target.val] = 1
         self.dfs(graph,ans,k,target.val,vis)
-        
         return ans
 
-
-        # return []
     def dfs(self,graph,ans,k,target,vis):
         if(k == 0):
             ans.append(target)
             return
-
-        temp = graph[target]
-        for i in temp:
+        for i in graph[target]:
             if(vis[i] == -1):
                 vis[i] = 1
                 self.dfs(graph,ans,k-1,i,vis)
     def inOrder(self,root,graph):
-        if(root == None):
-            return 
+
         if(root.val not in graph):
             graph[root.val] = []
         if(root.left):
             graph[root.val].append(root.left.val)
             graph[root.left.val] = [root.val]
+            self.inOrder(root.left,graph)
         if(root.right):
             graph[root.val].append(root.right.val)
             graph[root.right.val] = [root.val]
-        self.inOrder(root.left,graph)
-        self.inOrder(root.right,graph)
+            self.inOrder(root.right,graph)
+        
+        
 
 
             

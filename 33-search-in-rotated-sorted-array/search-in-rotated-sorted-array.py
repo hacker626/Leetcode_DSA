ce6211e83edad7e8d93da88966b14cde876b0382
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-23 14:54:12 +0530

    Time: 22 ms (88.75%) | Memory: 13.5 MB (65.16%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,10 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        n=len(nums)
-        
-        def bSearch(arr,s,e,target):
 
+        n = len(nums)
+        low = 0
+        high = n-1
+        def binaryS(nums,s,e):
 
             while(s<=e):
                 mid = s+(e-s)//2
@@ -18,30 +19,36 @@ class Solution(object):
                     e=mid-1
                 else:
                     s=mid+1
-            return -1
+            return float('inf')
         def rotate(nums):
-
+            low,high=0,n-1
             if(nums[0]<nums[-1]):
                 return 0
-            low,high = 0,len(nums)-1
-            # k = -1
+            
             while(low<=high):
-                mid = low+(high-low)//2
+                mid=(low)+(high-low)//2
+
                 if(0<mid<n-1 and nums[mid]<nums[mid-1] and nums[mid]<nums[mid+1]):
                     return mid
                 elif(nums[mid]<nums[-1]):
                     high=mid-1
                 else:
                     low=mid+1
-            return len(nums)-1
+            return n-1
+
         k = rotate(nums)
+
         if(nums[k] == target):
             return k
-        temp1 = bSearch(nums,0,k-1,target)
-        if(temp1!=-1):
+        temp1=binaryS(nums,0,k-1)
+        if(temp1 !=float('inf')):
             return temp1
-        temp2 = bSearch(nums,k+1,n-1,target)
-        if(temp2!=-1):
+        
+
+        temp2 = binaryS(nums,k+1,n-1)
+        if(temp2 !=float('inf')):
             return temp2
         return -1
+
+
         

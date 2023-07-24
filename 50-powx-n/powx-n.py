e73090e9fe0b2b89af6e596d5345221f6dddb395
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-24 13:28:49 +0530

    Time: 9 ms (95.79%) | Memory: 13.4 MB (34.84%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -5,12 +5,20 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        # return x**n
-        tempo = n
-        n=abs(n)
+
         if(n == 0):
             return 1
-        temp = self.myPow(x*x,(n//2))
-        if(n%2 != 0):
-            temp*=x 
-        return float(temp) if(tempo>=0) else float(1/temp)
+        temp = self.myPow(x*x,abs(n)//2)
+        if(abs(n)%2 !=0):
+            temp*=x
+        return float(temp) if(n>=0) else float(1/temp)
+    
+        # tempo = n
+        # n=abs(n)
+        # if(n == 0):
+        #     return 1
+        # temp = self.myPow(x*x,(n//2))
+        # if(n%2 != 0):
+        #     temp*=x 
+        # return float(temp) if(tempo>=0) else float(1/temp)
+        

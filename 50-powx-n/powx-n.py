b8fcef65e824a9cb3bf80c2c7ff349e8f3a81a1f
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-24 14:10:12 +0530

    Time: 19 ms (53.68%) | Memory: 13.2 MB (93.50%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -14,7 +14,8 @@ class Solution(object):
         # return float(temp) if(n>=0) else float(1/temp)
         if(n == 0):
             return 1
-        temp = self.myPow(x*x,(abs(n)//2))
+        temp = self.myPow(x,(abs(n)//2))
+        temp*=temp
         if(abs(n)%2 != 0):
             temp*=x 
         return float(temp) if(n>=0) else float(1/temp)

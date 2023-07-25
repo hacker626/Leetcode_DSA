3a0d91cada82ec562b25669807a33f83034a56c8
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-25 18:23:23 +0530

    Time: 530 ms (77.59%) | Memory: 25.6 MB (90.39%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
-        n = len(arr)
-        low,high = 0,n-1
+
+        low,high = 0,len(arr)-1
 
         while(low<=high):
-            mid=low+(high-low)//2
+            mid = low+(high-low)//2
 
-            if(arr[mid]>arr[mid-1] and arr[mid]>arr[mid+1]):
+            if(0<mid<len(arr)-1 and arr[mid]>arr[mid-1] and arr[mid]>arr[mid+1]):
                 return mid
-            elif(mid>0 and arr[mid]<arr[mid-1]):
+            elif(mid<len(arr)-1 and arr[mid]>arr[mid+1]):
                 high=mid-1
             else:
                 low=mid+1
-        # return high
-            
+        return 0
+        

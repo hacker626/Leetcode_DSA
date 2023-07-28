94475940cfe9ddec25a135a394031630a498d8cf
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-28 13:16:43 +0530

    Time: 19 ms (87.18%) | Memory: 13.4 MB (32.05%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -3,28 +3,26 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
-        """
-
+        """ 
         dp = {}
-        n1,n2 = 0,len(nums)-1
-        temp = self.dfs(nums,n1,n2,dp)
-        return (temp>=sum(nums)-temp)
-        
+        ans = self.dfs(nums,0,len(nums)-1,dp)
+        return ans>=sum(nums)-ans
+
+
 
     def dfs(self,nums,n1,n2,dp):
+
         if(n1>n2):
             return 0
         if(n1 == n2):
             return nums[n1]
-
         if(n1 in dp):
             if(n2 in dp[n1]):
                 return dp[n1][n2]
-
-        ans = max(nums[n1]+min(self.dfs(nums,n1+2,n2,dp),self.dfs(nums,n1+1,n2-1,dp)),nums[n2]+min(self.dfs(nums,n1+1,n2-1,dp),self.dfs(nums,n1,n2-2,dp)))
-
+        
+        temp = max(nums[n1]+min(self.dfs(nums,n1+2,n2,dp),self.dfs(nums,n1+1,n2-1,dp)),nums[n2]+min(self.dfs(nums,n1+1,n2-1,dp),self.dfs(nums,n1,n2-2,dp)))
         if(n1 not in dp):
             dp[n1] = {}
-        dp[n1][n2] = ans
+        dp[n1][n2] = temp
         return dp[n1][n2]
-
+        

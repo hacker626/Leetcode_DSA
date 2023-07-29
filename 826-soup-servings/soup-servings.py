154653de480406f02d8e48150363c3ca9fca5cfe
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-29 11:09:15 +0530

    Time: 11 ms (100.00%) | Memory: 14 MB (57.14%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -7,8 +7,6 @@ class Solution(object):
         """
         if(n>4800):
             return 1.0
-
-
         n = math.ceil(n/25.0)
         dp = {}
         return self.recMem(n,dp,n,n)
@@ -16,11 +14,11 @@ class Solution(object):
     def recMem(self,n,dp,i,j):
 
         if(i<=0 and j<=0):
-            return 0.5
+            return 0.5 #here we have to calculate the half of probability that both got empty
         elif(i<=0):
-            return 1.0
+            return 1.0 #here we have to return that the exact probability that A got emptied
         elif(j<=0):
-            return 0.0
+            return 0.0 # Since if B is emptied then both upper case cannot be possible thereafter.
         if((i,j) in dp):
             return dp[(i,j)]
         dp[(i,j)] = 0.25*(self.recMem(n,dp,i-4,j)+self.recMem(n,dp,i-3,j-1)+self.recMem(n,dp,i-2,j-2)+self.recMem(n,dp,i-1,j-3))

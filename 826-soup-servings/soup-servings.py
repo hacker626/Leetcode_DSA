5097331f1da5957d9aab22b3da603fea45942b81
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-29 10:59:58 +0530

    Time: 23 ms (92.86%) | Memory: 13.9 MB (64.29%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -1,20 +1,29 @@
-class Solution:
+import math
+class Solution(object):
     def soupServings(self, n):
-        if n > 4800: 
+        """
+        :type n: int
+        :rtype: float
+        """
+        if(n>4800):
             return 1.0
-        n = (n + 24) // 25
-        memo = dict()
+
+
+        n = math.ceil(n/25.0)
+        dp = {}
+        return self.recMem(n,dp,n,n)
+
+    def recMem(self,n,dp,i,j):
+
+        if(i<=0 and j<=0):
+            return 0.5
+        elif(i<=0):
+            return 1.0
+        elif(j<=0):
+            return 0.0
+        if((i,j) in dp):
+            return dp[(i,j)]
+        dp[(i,j)] = 0.25*(self.recMem(n,dp,i-4,j)+self.recMem(n,dp,i-3,j-1)+self.recMem(n,dp,i-2,j-2)+self.recMem(n,dp,i-1,j-3))
+        return dp[(i,j)]
+
         
-        def dp(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-            if i <= 0 and j <= 0: 
-                return 0.5
-            if i <= 0: 
-                return 1.0
-            if j <= 0: 
-                return 0.0
-            memo[(i, j)] = 0.25 * (dp(i - 4, j) + dp(i - 3, j - 1) + dp(i - 2, j - 2) + dp(i - 1, j - 3))
-            return memo[(i, j)]
-        
-        return dp(n, n)

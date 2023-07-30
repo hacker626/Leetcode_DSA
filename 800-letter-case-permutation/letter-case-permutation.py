b36f93ea00be58d031f2d43a619864cc437cba36
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-30 20:57:19 +0530

    Time: 42 ms (67.86%) | Memory: 14.1 MB (69.64%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -16,15 +16,17 @@ class Solution(object):
             if(s not in temp):
                 temp[s] = 1
             return
-        str1 = s
+        # str1 = s
         
         if(65<=ord(s[n-1])<=90):
             str1 = s[:n-1]+s[n-1].lower()+s[n:]
+            self.func(str1,temp,n-1)
         elif(97<=ord(s[n-1])<=122):
             str1 = s[:n-1]+s[n-1].upper()+s[n:]
+            self.func(str1,temp,n-1)
         # if(n == 1):
         #     print(str1)
-        self.func(str1,temp,n-1)
+        
         # if(str1 not in temp):
         #     temp[str1] = 1
         self.func(s,temp,n-1)

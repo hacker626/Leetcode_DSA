abb2c97f65d647eeb0850b7dd1dd493e86c09635
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-30 20:57:00 +0530

    Time: 49 ms (38.39%) | Memory: 14.1 MB (61.61%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -13,6 +13,8 @@ class Solution(object):
 
 
         if(n == 0):
+            if(s not in temp):
+                temp[s] = 1
             return
         str1 = s
         
@@ -23,10 +25,9 @@ class Solution(object):
         # if(n == 1):
         #     print(str1)
         self.func(str1,temp,n-1)
-        if(str1 not in temp):
-            temp[str1] = 1
+        # if(str1 not in temp):
+        #     temp[str1] = 1
         self.func(s,temp,n-1)
-        if(s not in temp):
-            temp[s] = 1
+        
         
         

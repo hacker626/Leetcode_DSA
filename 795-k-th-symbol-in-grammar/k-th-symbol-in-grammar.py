64626727eebf23273ac15b6c6ddd53a6d31c9ed0
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-30 15:57:30 +0530

    Time: 8 ms (97.40%) | Memory: 13.3 MB (23.38%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -5,18 +5,11 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # return 1 if
 
-        return 1 if(self.solve(n,k)) else 0
-
-    def solve(self,n,k):
-
-        if(n == 1 and k == 1):
+        if(n == 1):
             return 0
         if(k<=(2**(n-2))):
-            return self.solve(n-1,k)
+            return self.kthGrammar(n-1,k)
         else:
-            return not self.solve(n-1,k-(2**(n-2)))
-
+            return 1^self.kthGrammar(n-1,k-(2**(n-2)))
         
-

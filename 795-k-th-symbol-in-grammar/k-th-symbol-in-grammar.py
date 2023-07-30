fa401c718b8303c87c01c09abca9d09d6aad1162
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-30 18:09:50 +0530

    Time: 24 ms (7.79%) | Memory: 13.2 MB (57.14%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -8,9 +8,11 @@ class Solution(object):
 
         if(n == 1):
             return 0
+
+
         if(k<=(2**(n-2))):
             return self.kthGrammar(n-1,k)
         else:
-            return 1^self.kthGrammar(n-1,k-2**(n-2))
+            return 1^self.kthGrammar(n-1,k-(2**(n-2)))
 
         

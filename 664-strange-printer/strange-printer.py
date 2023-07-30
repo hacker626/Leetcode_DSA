d1b0d098fc3383c67a68484d6964297193d736df
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-30 11:06:04 +0530

    Time: 248 ms (100.00%) | Memory: 17.4 MB (25.00%) - LeetSync

---
+++ 664-strange-printer/strange-printer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def strangePrinter(self, s):
+        
+        memo = {}
+        def check(i, j):
+            if (i, j) not in memo:
+                if i > j: return 0
+                
+                res = check(i, j-1) + 1
+                for k in range(i, j):
+                    if s[k] == s[j]:
+                        res = min(res, check(i, k-1) + check(k, j-1))
+                memo[(i, j)] = res
+                
+            return memo[(i, j)]
+        
+        s = re.sub(r'(.)\1*', r'\1', s)
+        return check(0, len(s) - 1)

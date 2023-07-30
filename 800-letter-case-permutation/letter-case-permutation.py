3d6fbfb10448ce5e0adcefd39ec9d383c592702c
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-07-30 20:49:32 +0530

    Time: 51 ms (29.46%) | Memory: 14.1 MB (61.61%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def letterCasePermutation(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        temp = {}
+        self.func(s,temp,len(s))
+        return temp.keys()
+
+
+    def func(self,s,temp,n):
+
+
+        if(n == 0):
+            return
+        str1 = s[:]
+        
+        if(65<=ord(s[n-1])<=90):
+            str1 = s[:n-1]+s[n-1].lower()+s[n:]
+        elif(97<=ord(s[n-1])<=122):
+            str1 = s[:n-1]+s[n-1].upper()+s[n:]
+        if(n == 1):
+            print(str1)
+        self.func(str1,temp,n-1)
+        if(str1 not in temp):
+            temp[str1] = 1
+        self.func(s,temp,n-1)
+        if(s not in temp):
+            temp[s] = 1
+        
+        

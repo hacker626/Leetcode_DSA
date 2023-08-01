c1cfd2d2653b2722fe5b099635726bd9348eb423
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-08-01 14:28:13 +0530

    Time: 1109 ms (95.94%) | Memory: 27 MB (70.90%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -4,28 +4,28 @@ class Solution(object):
         :type temperatures: List[int]
         :rtype: List[int]
         """
+        
 
-        stack = []
-        ans = []
+        stack,ans = [],[]
 
         for i in range(len(temperatures)-1,-1,-1):
 
             while(len(stack)>0 and temperatures[stack[-1]]<=temperatures[i]):
                 stack.pop()
-            
+
+                        
+
             if(len(stack) == 0):
                 ans.append(-1)
             else:
                 ans.append(stack[-1])
+
             stack.append(i)
         ans.reverse()
-        # print(ans)
 
         for i in range(len(temperatures)):
-            if(ans[i] != -1):
-                ans[i] = ans[i]-i
+            if(ans[i]!=-1):
+                ans[i]-=i
             else:
                 ans[i] = 0
         return ans
-
-        

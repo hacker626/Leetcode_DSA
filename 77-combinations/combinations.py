d6191595082dbf7042d865480ab8e379e9d02fb2
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-08-01 08:49:41 +0530

    Time: 366 ms (75.19%) | Memory: 14.6 MB (97.03%) - LeetSync

---
+++ 77-combinations/combinations.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+
+        ans = []
+
+        self.func(n,k,ans,[],1)
+        return ans
+
+    def func(self,n,k,ans,temp,start):
+
+        if(len(temp) == k):
+            ans.append(temp)
+            return
+
+
+        for i in range(start,n+1):
+            temp1 = temp+[i]
+            self.func(n,k,ans,temp1,i+1)
+        pass

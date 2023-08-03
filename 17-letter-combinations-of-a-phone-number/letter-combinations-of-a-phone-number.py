ffac40b8b0dac5dff5d9145b3a580a218652b90b
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-08-03 12:53:52 +0530

    Time: 16 ms (61.65%) | Memory: 13.5 MB (11.19%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -8,9 +8,7 @@ class Solution(object):
             return []
         dict_ = {2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
         ans = []
-
         return self.func(digits,dict_,0,"",ans)
-
     def func(self,digits,dict_,n,temp,ans):
         if(len(temp) == len(digits)):
             ans.append(temp)
@@ -19,7 +17,7 @@ class Solution(object):
         #     return
 
         for i in dict_[int(digits[n])]:
-            tempo= temp+i
-            self.func(digits,dict_,n+1,tempo,ans)
+            # tempo= temp+i
+            self.func(digits,dict_,n+1,temp+i,ans)
         return ans
 

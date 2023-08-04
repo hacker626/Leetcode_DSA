4f445ae85c31dd0802f9da6720bfb1085252a92d
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-08-04 13:08:58 +0530

    Time: 39 ms (97.30%) | Memory: 16.7 MB (10.67%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,12 +1,32 @@
-from functools import lru_cache #added this
+from functools import lru_cache
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        @lru_cache(None) # and this
-        def helper(prev):
-            if prev=="":return True
-            for word in wordDict:
-                if prev.startswith(word):
-                    i=len(word)
-                    if helper(prev[i:]):return True
+        @lru_cache(None)
+
+        def func(S):
+
+            if(S == ""):
+                return True
+            
+            for i in wordDict:
+                if S.startswith(i):
+                    temp = len(i)
+                
+                    if(func(S[temp:])):
+                        return True
             return False
-        return helper(s)
+        return func(s)
+
+# from functools import lru_cache #added this
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+#         @lru_cache(None) # and this
+#         def helper(prev):
+#             if prev=="":return True
+#             for word in wordDict:
+#                 if prev.startswith(word):
+#                     i=len(word)
+#                     if helper(prev[i:]):return True
+#             return False
+#         return helper(s)

19dbf85a350a2d5ec1595d1e6193335529841f77
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-08-08 12:37:32 +0530

    Time: 16 ms (97.99%) | Memory: 13.5 MB (59.16%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,57 +1,50 @@
 class Solution(object):
+    def binarySearch(self,arr,low,high,target):
+
+        while(low<=high):
+            mid = low+(high-low)//2
+            if(arr[mid] == target):
+                return mid
+            elif(arr[mid]>target):
+                high=mid-1
+            else:
+                low=mid+1
+        return -1
     def search(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
+        low,high = 0,len(nums)-1
+        index = self.findIndex(nums,low,high)
+        # print(index)
+        tempo = self.binarySearch(nums,0,index-1,target)
 
-        n = len(nums)
-        low = 0
-        high = n-1
-        def binaryS(nums,s,e):
-
-            while(s<=e):
-                mid = s+(e-s)//2
-                if(nums[mid] == target):
-                    return mid
-                elif(nums[mid]>target):
-                    e=mid-1
-                else:
-                    s=mid+1
-            return float('inf')
-        def rotate(nums):
-            low,high=0,n-1
-            if(nums[0]<nums[-1]):
-                return 0
-            
-            while(low<=high):
-                mid=(low)+(high-low)//2
-
-                if(0<mid<n-1 and nums[mid]<nums[mid-1] and nums[mid]<nums[mid+1]):
-                    return mid
-                elif(nums[mid]<nums[-1]):
-                    high=mid-1
-                else:
-                    low=mid+1
-            return n-1
-
-        k = rotate(nums)
-
-        if(nums[k] == target):
-            return k
-        temp1=binaryS(nums,0,k-1)
-        if(temp1 !=float('inf')):
-            return temp1
-        
-
-        temp2 = binaryS(nums,k+1,n-1)
-        if(temp2 !=float('inf')):
-            return temp2
+        if(tempo!=-1):
+            return tempo
+        tempo2 = self.binarySearch(nums,index,high,target)
+        # print(tempo,tempo2)
+        if(tempo2!=-1):
+            return tempo2
         return -1
 
 
 
 
+    def findIndex(self,nums,low,high):
+
+        if(nums[0]<=nums[-1]):
+            return 0
+        low,high = 0,len(nums)-1
+        while(low<=high):
+            mid = low+(high-low)//2
+            if(0<mid<len(nums)-1 and nums[mid-1]>nums[mid] and nums[mid]<nums[mid+1]):
+                return mid
+            elif(nums[mid]<nums[-1]):
+                high=mid-1
+            else:
+                low=mid+1
+        return high
 
         

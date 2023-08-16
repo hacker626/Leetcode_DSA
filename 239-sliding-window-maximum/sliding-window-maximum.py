fe5c75d61bdb6b38388e315bf4773bac0b29f2ce
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-08-16 11:55:26 +0530

    Time: 1834 ms (14.17%) | Memory: 30.2 MB (94.43%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -9,6 +9,9 @@ class Solution(object):
         maxNum = []
         ans = []
         while(high<len(nums)):
+            #here maxNum[-1]<=nums[high] cannot be taken because if considered like this then we will pop out the old num[high] if repitition caused and it will ultimately deletes out the remaining nums[high] if windows slides forward.
+            #here 7 is maximum in slides for example [-7,-8,7,5,7,1,6,0] if second 7 is encoutered we are gonna deleting the old one, and when windows slides forwards; we have to delete out the maximum element which is 7.It means both the 7 will be delete out.
+            #Therefore ensure that the leftmost element of maxNum is maximum and leftmost in nums.
             while(len(maxNum)>0 and maxNum[-1]<nums[high]):
                 maxNum.pop()
             maxNum.append(nums[high])

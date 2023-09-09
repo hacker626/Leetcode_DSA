584b5ee647d003e2597470b5cdf1b3de8f68050b
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-09-09 21:42:06 +0530

    Time: 26 ms (42.89%) | Memory: 13.3 MB (67.68%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -5,18 +5,24 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        dp = [-1 for i in range(target+1)]
-        return self.func(nums,target,dp)
-    def func(self,nums,target,dp):
-        if(target == 0):
-            return 1
-        elif(target<0):
-            return 0
-        if(dp[target] != -1):
-            return dp[target]
-        dp[target] = 0
-        for i in range(len(nums)):
-            dp[target] += self.func(nums,target-nums[i-1],dp)
+        dp = [0 for i in range(target+1)]
+        dp[0] = 1
+        for i in range(1,target+1):
             
-        return dp[target]
+            for j in range(len(nums)):
+                if(i-nums[j-1]>=0):
+                    dp[i]+=dp[i-nums[j-1]]
+        return dp[-1]
+    # def func(self,nums,target,dp):
+    #     if(target == 0):
+    #         return 1
+    #     elif(target<0):
+    #         return 0
+    #     if(dp[target] != -1):
+    #         return dp[target]
+    #     dp[target] = 0
+    #     for i in range(len(nums)):
+    #         dp[target] += self.func(nums,target-nums[i-1],dp)
+            
+    #     return dp[target]
         

dfef9602c08e7b6395ac0ab5c4d296fd7084f8f7
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-11-09 09:19:25 +0530

    Time: 87 ms (80.70%) | Memory: 18.1 MB (10.53%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
@@ -1,12 +1,12 @@
 class Solution(object):
     # def __init__(self):
     #     self.dict_ = {}
-    def funcsum(self,count):
-        if(count == 0):
-            return 0
-        # if(count in self.dict_):
-        #     return self.dict_[count]
-        return (count+self.funcsum(count-1))%(10**9+7)
+    # def funcsum(self,count):
+    #     if(count == 0):
+    #         return 0
+    #     # if(count in self.dict_):
+    #     #     return self.dict_[count]
+    #     return (count+self.funcsum(count-1))%(10**9+7)
         # return self.dict_[count]
     def countHomogenous(self, s):
         """
@@ -20,11 +20,11 @@ class Solution(object):
             if(s[i] == prev):
                 count+=1
             else:
-                ans+=self.funcsum(count)
-                print(ans)
+                ans+=(((count*(count+1))//2)%(10**9+7))
+                # print(ans)
                 prev = s[i]
                 count = 1
-        return ans+self.funcsum(count)
+        return (ans+(count*(count+1)//2))%(10**9+7)
 
 
         

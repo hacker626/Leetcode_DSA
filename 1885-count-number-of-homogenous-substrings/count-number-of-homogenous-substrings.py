38fcc538f2f39c09e3ef185bb663a3bca6f2de1a
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2023-11-09 09:16:53 +0530

    Time: 315 ms (5.26%) | Memory: 91.3 MB (5.26%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
@@ -1,13 +1,13 @@
 class Solution(object):
-    def __init__(self):
-        self.dict_ = {}
+    # def __init__(self):
+    #     self.dict_ = {}
     def funcsum(self,count):
         if(count == 0):
             return 0
-        if(count in self.dict_):
-            return self.dict_[count]
-        self.dict_[count] =  (count+self.funcsum(count-1))%(10**9+7)
-        return self.dict_[count]
+        # if(count in self.dict_):
+        #     return self.dict_[count]
+        return (count+self.funcsum(count-1))%(10**9+7)
+        # return self.dict_[count]
     def countHomogenous(self, s):
         """
         :type s: str

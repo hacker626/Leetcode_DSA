b1b83d36bf3641c1ac4167bc3fef2deb0553906d
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2024-01-03 14:00:02 +0530

    Time: 289 ms (15.91%) | Memory: 17.4 MB (5.68%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def numberOfBeams(self, bank):
+        """
+        :type bank: List[str]
+        :rtype: int
+        """
+        ans = 0
+        temp = [0 for i in range(len(bank))]
+        for i in range(len(bank)):
+            for j in range(len(bank[i])):
+                if(bank[i][j] == '1'):
+                    temp[i]+=1
+            # if(temp[i] == 0):
+            #     temp.pop(i)
+        
+        print(temp)
+        tempo = []
+        for i in temp:
+            if(i!=0):
+                tempo.append(i)
+        i,j = 0,1
+        while(j<len(tempo)):
+            ans+=(tempo[j]*tempo[i])
+            j+=1
+            i+=1
+        return ans
+        

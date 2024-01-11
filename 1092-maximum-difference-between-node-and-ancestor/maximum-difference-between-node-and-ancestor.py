d6400296e3e5699db909c8886a0d6c02e5d02737
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2024-01-11 15:43:15 +0530

    Time: 18 ms (77.88%) | Memory: 19.9 MB (25.66%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
@@ -10,44 +10,16 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        ans1 = [float('-inf')]
-        ans2 = [float('-inf')]
-        self.func1(root,ans1)
-        self.func2(root,ans2)
-
-        return max(ans1[0],ans2[0])
-
-    def func1(self,root,ans):
-        if(root == None):
-            return float('inf')
-
-        temp = min(root.val,self.func1(root.left,ans),self.func1(root.right,ans))
-        ans[0] = max(ans[0],abs(root.val-temp))
-        return temp
-
-    def func2(self,root,ans):
-        if(root == None):
-            return float('-inf')
-
-        temp = max(root.val,self.func2(root.left,ans),self.func2(root.right,ans))
-        ans[0] = max(ans[0],abs(root.val-temp))
-        return temp
         
-
-
-        
-        
-        # self.func(root.left,ans,small,large)
-
-
-        # temp = min(self.func())
-        # if(root.val<small):
-        #     small = root.val
-        # if(root.val>large):
-        #     large = root.val
-        # ans[0] = max(ans[0],abs(root.val-large),abs(root.val-small))
-        # print(ans)
-        # self.func(root.right,ans,small,large)
+        def func(root,low,high,ans):
+            if(root == None):
+                return
+            ans[0] = max(ans[0],abs(root.val-low),abs(root.val-high))
+            func(root.left,min(low,root.val),max(high,root.val),ans)
+            func(root.right,min(low,root.val),max(high,root.val),ans)
+        ans = [0]
+        func(root,root.val,root.val,ans)
+        return ans[0]
 
 
         

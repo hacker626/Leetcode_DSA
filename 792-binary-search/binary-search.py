386627a77c908ce65c7b28de624e2ef9a792166a
Mridul Bhaskar <66233331+hacker626@users.noreply.github.com>
2024-01-13 17:18:35 +0530

    Time: 192 ms (18.51%) | Memory: 14.5 MB (49.09%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -5,15 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        low,hi = 0,len(nums)-1
-
-        while(low<=hi):
-            mid = low+(hi-low)//2
-
-            if(nums[mid] == target):
+        low,high = 0,len(nums)-1
+        while(low<=high):
+            mid = low+(high-low)//2
+            if(nums[mid]>target):
+                high=mid-1
+            elif(nums[mid] == target):
                 return mid
-            elif(nums[mid]>target):
-                hi = mid-1
             else:
                 low = mid+1
         return -1
